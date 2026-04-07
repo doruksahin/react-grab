20d7e4381398491bc808b5866a98801b041963f3
doruk <doruksahin98@gmail.com>
2026-04-07 15:43:32 +0300

    refactor(core): thread ShadowRoot from mountRoot to ReactGrabRenderer prop
    The wiring layer destructures the ShadowMountResult and passes shadowRoot
    explicitly. No component downstream needs to call getRootNode() anymore.

--- packages/react-grab/src/core/index.tsx
+++ packages/react-grab/src/core/index.tsx
@@ -3321,7 +3321,7 @@ export const init = (rawOptions?: Options): ReactGrabAPI => {
     });
 
     const resolvedCssText = typeof cssText === "string" ? cssText : "";
-    const rendererRoot = mountRoot(resolvedCssText);
+    const { root: rendererRoot, shadowRoot } = mountRoot(resolvedCssText);
 
     const isThemeEnabled = createMemo(() => pluginRegistry.store.theme.enabled);
     const isSelectionBoxThemeEnabled = createMemo(
@@ -4338,6 +4338,7 @@ export const init = (rawOptions?: Options): ReactGrabAPI => {
           disposeRenderer = render(() => {
             return (
               <ReactGrabRenderer
+                shadowRoot={shadowRoot}
                 selectionVisible={selectionVisible()}
                 selectionBounds={selectionBounds()}
                 selectionBoundsMultiple={selectionBoundsMultiple()}
